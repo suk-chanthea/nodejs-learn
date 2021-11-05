2595eef2664b6c7e91fe1e7b7973de47ebb34822
suk-chanthea <sukchanthea@gmail.com>
2021-11-05 15:38:15 +0700

    first-commit

---
+++ 2-global.js
@@ -0,0 +1,12 @@
+// GLOBALS -No WINDOW !!!
+// __dirname - path to current directory
+// __filename - File name
+// require   - function to use modules (CommonJS)
+// module    - info about current module (file)
+// process   - info about env where the program is being executed
+
+console.log(__dirname)
+console.log(__filename)
+setInterval(()=>{
+    console.log("Welcome Chanthea")
+},1000)
